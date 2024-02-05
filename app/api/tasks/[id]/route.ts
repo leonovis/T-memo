abf078ec4653bfbee7cc7d23ca4496ff2362a9ee
Leonard Akpofure <la.shivure@gmail.com>
2024-02-05 11:02:28 +0100

    Remove unused EDIT function from route.ts

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -26,33 +26,3 @@ export async function DELETE(
     return NextResponse.json({ error: "Error deleting task", status: 500 });
   }
 }
-
-export async function EDIT(req: Request) {
-  try {
-    const { userId } = auth();
-    const { id, data } = await req.json();
-
-    if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
-
-    const task = await prisma.task.update({
-      where: {
-        id,
-      },
-      data: {
-        title: data.title,
-        description: data.description,
-        date: data.date,
-        isCompleted: data.completed,
-        isImportant: data.important,
-
-      },
-    });
-
-    return NextResponse.json(task);
-  } catch (error) {
-    console.log("ERROR UPDATING TASK: ", error);
-    return NextResponse.json({ error: "Error updating task", status: 500 });
-  }
-}
